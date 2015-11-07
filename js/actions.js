dc1f4e4fe4db7011b9f16db4d5a8e4ab944c2066
0rbeg <gortegab@gmail.com>
2015-11-07 11:33:17 -0600

    07112015

--- js/actions.js
+++ js/actions.js
@@ -1,14 +1,16 @@
 var fn = {
 	init: function(){
-	//	if(!fn.islogged())
-	//		window.location.href = "#reg";
-           // alert('Aqui!!');
+		if(!fn.islogged())
+			window.location.href = "#reg";
+           
 		
 		//Funcionalidades de Registro
-        window.location.href = "#nr2";
+        window.location.href = "#nr1";
 		$('#regSend').click(fn.getRegister);
 		$('#takePhoto').click(mediaCapture.takePhoto);
         $('#nr1 ul[data-role=listview] a').click(fn.seleccionarTipo);
+        $('#nr1 div[data-role=navbar] li').click(fn.nr1Siguiente);
+        /*$('#nr2 div[data-role=navbar] li').click(fn.nr2Reservar);*/
 	},
 	ready:function(){
 		document.addEventListener("deviceready", fn.init, false);
@@ -29,9 +31,40 @@ var fn = {
 	},
     seleccionarTipo: function(){
         $(this).parents("ul").find("a").removeClass('ui-btn-active');
-        console.log("Hola");
         $(this).addClass("ui-btn-active");
+        $("#nr1").attr("th",$(this).text());
+    },
+    nr1Siguiente: function(){
+    if($(this).index() == 1 && $("#nr1").attr("th") != undefined){
+        /*Opcion seleccionada, pasar a la siguinete pantalla */
+        window.location.href="#nr2";
+        
+    }else{
+        if($(this).index() != 0){
+        alert("Es necesario seleccionar un tipo de habitacion");
+        }
     }
+        
+    },
+    nr2Reservar: function(){
+        var th = $("nr1").attr("th");
+        var np = $("#numPersonas").val();
+        var nh = $("#numHabitaciones").val();
+        var nd = $("#numDias").val();
+        
+        if(conexion.estaConectado(){
+            //Enviar información al servidor
+           server.envRes(th,np,nh,nd);
+        }else{
+            //almacenar en el dispositivo
+           $.mobile.loading("show");
+           almacen.guardarReserva(th,np,nh,nd);
+           $.mobile.loading("hide");
+               
+        }
+        
+    }
+}
 };
     
 $(fn.ready);
